b36f1c9cfc84b877e5c97a13044efbc3d49b83f5
Omar Safwany <omar.safwany@gmail.com>
2017-10-02 20:21:23 +0200

    added assets to deploy

--- eventtusTask/app/assets/javascripts/package.js
+++ eventtusTask/app/assets/javascripts/package.js
@@ -1,6 +1,6 @@
-$( document ).ready(function() {
+// jQuery(document).ready(function($) {
     console.log( "ready!" );
     $("#add").on('click', function(e){
         $("#dependenciesDiv").append('<div><input type="text" name="package[dependencies][]"><a onclick="$(this).parent().remove()">x</a></div>');
-    })
-});
+    });
+// });
